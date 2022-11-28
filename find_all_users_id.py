1cbda88e4bbda3097379e9fc52019da09e9d2bf2
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-11-28 09:36:42 +0500

    1 misol

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -9,4 +9,10 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    users=data["messages"]
+    ids=[]
+    for i in users:
+        ids = i.get('id')
+    return ids
+data=read_data('data/result.json')
+print(find_all_users_id(data))
